8169ed42a3bbf718bd16782daebb71d30664a49f
Abhishek Suman <abhisuma@amazon.com>
2024-09-20 20:01:05 +0530

    second commitah

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,8 +2,16 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { sayHello } from './functions/say-hello/resource';
-defineBackend({
+import { aws_lambda } from 'aws-cdk-lib';
+const backend= defineBackend({
   auth,
   data,
   sayHello,
 });
+
+const amplifyBackendName = backend.auth.resources.userPool.node.tryGetContext(
+  "amplify-backend-name"
+);
+
+const sayHelloLambda = backend.sayHello.resources.lambda as aws_lambda.Function
+sayHelloLambda.addEnvironment("SQS_QUEUE_URL", amplifyBackendName);
